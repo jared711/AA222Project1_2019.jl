b426c28ef0a89a02e691e4808f2b29b750c7d71b
Jared Blanchard <jaredb711@gmail.com>
2019-04-12 16:40:31 -0700

    b

--- project1.jl
+++ project1.jl
@@ -1,60 +1,25 @@
-using Pkg
-Pkg.add("Plots")
-Pkg.add("PyPlot")
-using Plots
-pyplot(size = (900,900), legend = false)
-
 using LinearAlgebra
 using Statistics
 
-X = -15:0.1:15
-Y = -15:0.1:15
-Z = zeros(length(X), length(Y))
-for i in 1: length(X)
-    for j in 1: length(Y)
-        x_state = [X[i], Y[j]]
-        Z[i,j] = rosenbrock(x_state)
-    end
-end
-
-contour(X,Y,Z)
-scatter!([1,1],[1,1],marker = (:circle, 5, 0.6, :green, stroke(3, 0.2, :green, :dot)))
-
-x0 = [0.0,0.0]
-optimize(rosenbrock,myfun,x0,100,"hello")
-
-function myfun(x)
-    return x
-end
-
 function optimize(f, g, x0, n, prob)
-    if prob == "secret_2"
-        #half random
-        x = x0
-        y = f(x0)
-        for i in 1 : floor(n/2)-1
-            x_next = x + randn(length(x))
-            y_next = f(x_next)
-            if y_next < y
-                x, y = x_next, y_next
-            end
-        end
-        #half hooke_jeeves
-        x = hooke_jeeves(f, x, 1.0, 0.01, floor(n/2))
+    if prob == "secret_2" || prob == "secret_1"
+        basis(i, m) = [k == i ? 1.0 : 0.0 for k in 1 : m]
+    
+        S = [basis(i, length(x0)) * x0[i] for i in 1:length(x0)]
+        push!(S, randn(length(x0)))
+    
+        x = nelder_mead(f, S, 1e-8, n)
+        return x
     else
         x = hooke_jeeves(f, x0, 1.0, 0.01, n)
     end
     return x
 end
 
-function hooke_jeeves(f, x, α, ϵ, counter, γ=0.5)     
+function hooke_jeeves(f, x, α, ϵ, counter, γ=0.5)
     basis(i, m) = [k == i ? 1.0 : 0.0 for k in 1 : m]
     while counter > 0
         y, n = f(x), length(x)
-        scatter!([x[1],x[1]],[x[2],x[2]],marker = (:circle, 5, 0.6, :magenta, stroke(3, 0.2, :magenta, :dot)))
-        #x_list[list_counter,:], f_list[list_counter] = x, y
-        #list_counter += 1
-
         counter -= 1
         while α > ϵ && counter > 2*n
             improved = false
@@ -63,10 +28,6 @@ function hooke_jeeves(f, x, α, ϵ, counter, γ=0.5)
                 for sgn in (-1, 1)
                     x′ = x + sgn*α*basis(i, n)
                     y′ = f(x′)
-                    scatter!([x'[1],x'[1]],[x'[2],x'[2]],marker = (:circle, 5, 0.6, :blue, stroke(3, 0.2, :blue, :dot)))
-                    #x_list[list_counter,:], f_list[list_counter] = x, y
-                    #list_counter += 1
-
                     counter -= 1
                     if y′ < y_best
                         x_best, y_best, improved = x′, y′, true
@@ -80,19 +41,51 @@ function hooke_jeeves(f, x, α, ϵ, counter, γ=0.5)
             end
         end
     end
-
-    scatter!([x[1],x[1]],[x[2],x[2]],marker = (:circle, 5, 0.6, :red, stroke(3, 0.2, :red, :dot)))
     return x
 end
+                                  
 
-function rosenbrock(x)
-    x_star = ([1,1])
-    f_star = 0
-    n = 20
-    x1 = x[1]
-    x2 = x[2]
-    f = 100*(x2-x1^2)^2 + (1-x1)^2;
-    return f
+function nelder_mead(f, S, ϵ, n; α=1.0, β=2.0, γ=0.5)
+    counter = n
+    Δ, y_arr = Inf, f.(S)
+    counter -= length(S)
+    while Δ > ϵ && counter > length(S) #+ 1 #maybe not necessary
+        p = sortperm(y_arr) #sort lowest to highest
+        S, y_arr = S[p], y_arr[p]
+        xl, yl = S[1], y_arr[1] #lowest
+        xh, yh = S[end], y_arr[end] #highest
+        xs, ys = S[end-1], y_arr[end-1] #second-highest
+        xm = mean(S[1:end-1]) #centroid
+        xr = xm + α*(xm - xh) #reflection point
+        yr = f(xr)
+        counter -= 1 #MINUS 1
+        
+        if yr < yl
+            xe = xm + β*(xr-xm) #expansion point
+            ye = f(xe)
+            counter -= 1 #MINUS 1
+            S[end], y_arr[end] = ye < yr ? (xe, ye) : (xr, yr)
+        elseif yr > ys
+            if yr ≤ yh
+                xh, yh, S[end], y_arr[end] = xr, yr, xr, yr
+            end
+            xc = xm + γ*(xh-xm) #contraction point
+            yc = f(xc)
+            counter -= 1 #MINUS 1
+            if yc > yh
+                for i in 2 : length(y_arr)
+                    S[i] = (S[i] + xl)/2
+                    y_arr[i] = f(S[i])
+                    counter -= length(y_arr)-1 #MINUS (length(y_arr)-1)
+                end
+            else
+                S[end], y_arr[end] = xc, yc
+            end
+        else
+            S[end], y_arr[end] = xr, yr
+        end
+
+        Δ = std(y_arr, corrected=false)
+    end
+    return S[argmin(y_arr)]
 end
-
-
