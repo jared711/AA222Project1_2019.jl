c671c719b0c4e8d85715db9300a831e55518da9d
Jared Blanchard <jaredb711@gmail.com>
2019-04-12 16:37:51 -0700

    all done proj1

---
+++ project1_nelder_mead.jl
@@ -0,0 +1,54 @@
+function optimize(f, g, x0, n, prob; α=1.0, β=2/0, γ=0.5)
+    basis(i, m) = [k == i ? 1.0 : 0.0 for k in 1 : m]
+    
+    S = [basis(i, length(x0)) * x0[i] for i in 1:length(x0)]
+    push!(S, randn(length(x0)))
+    
+    x = nelder_mead(f, S, 1e-8, n)
+    return x
+end
+
+function nelder_mead(f, S, ϵ, n; α=1.0, β=2.0, γ=0.5)
+    counter = n
+    Δ, y_arr = Inf, f.(S)
+    counter -= length(S)
+    while Δ > ϵ && counter > length(S) #+ 1 #maybe not necessary
+        p = sortperm(y_arr) #sort lowest to highest
+        S, y_arr = S[p], y_arr[p]
+        xl, yl = S[1], y_arr[1] #lowest
+        xh, yh = S[end], y_arr[end] #highest
+        xs, ys = S[end-1], y_arr[end-1] #second-highest
+        xm = mean(S[1:end-1]) #centroid
+        xr = xm + α*(xm - xh) #reflection point
+        yr = f(xr)
+        counter -= 1 #MINUS 1
+        
+        if yr < yl
+            xe = xm + β*(xr-xm) #expansion point
+            ye = f(xe)
+            counter -= 1 #MINUS 1
+            S[end], y_arr[end] = ye < yr ? (xe, ye) : (xr, yr)
+        elseif yr > ys
+            if yr ≤ yh
+                xh, yh, S[end], y_arr[end] = xr, yr, xr, yr
+            end
+            xc = xm + γ*(xh-xm) #contraction point
+            yc = f(xc)
+            counter -= 1 #MINUS 1
+            if yc > yh
+                for i in 2 : length(y_arr)
+                    S[i] = (S[i] + xl)/2
+                    y_arr[i] = f(S[i])
+                    counter -= length(y_arr)-1 #MINUS (length(y_arr)-1)
+                end
+            else
+                S[end], y_arr[end] = xc, yc
+            end
+        else
+            S[end], y_arr[end] = xr, yr
+        end
+
+        Δ = std(y_arr, corrected=false)
+    end
+    return S[argmin(y_arr)]
+end
